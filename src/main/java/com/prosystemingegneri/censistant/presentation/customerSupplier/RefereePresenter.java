6be9792a18bb6f0ca0ce757f90bffbdbc42d22cf
Davide Mainardi <ingmainardi@live.com>
2017-08-24 14:46:43 +0200

    Started to create functions for managing customers (and suppliers) referees.

--- src/main/java/com/prosystemingegneri/censistant/presentation/customerSupplier/RefereePresenter.java
+++ src/main/java/com/prosystemingegneri/censistant/presentation/customerSupplier/RefereePresenter.java
@@ -64,9 +64,9 @@ public class RefereePresenter implements Serializable {
     private String chooseReturnString() {
         String returnString;
         if (isCustomerView)
-            returnString = "/secured/customerSupplier/customers";
+            returnString = "/secured/customerSupplier/customer";
         else
-            returnString = "/secured/customerSupplier/suppliers";
+            returnString = "/secured/customerSupplier/supplier";
         
         return returnString + "?faces-redirect=true";
     }
