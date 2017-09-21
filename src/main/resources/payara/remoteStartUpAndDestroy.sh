23e8198b270591d5b7dc354b049a7efee4532328
Davide Mainardi <ingmainardi@live.com>
2017-09-21 10:47:45 +0200

    Updated script for remote destrying and start up.

--- src/main/resources/payara/remoteStartUpAndDestroy.sh
+++ src/main/resources/payara/remoteStartUpAndDestroy.sh
@@ -1,44 +1,64 @@
-scp /home/maina/NetBeansProjects/censistant/target/censistant.war root@192.168.2.252:/root/
+#!/bin/sh
 
-ssh root@192.168.2.252
+readonly APP_ID=1
+readonly DB_EXTERNAL_SSH_PORT=22
+readonly PAYARA_EXTERNAL_SSH_PORT=22
+readonly APP_NAME=censistant
+readonly DB_IP_ADDRESS=127.0.0.1
+readonly DB_USER_PASSWORD=PNpKjM19byxa6JBIKxFU
+readonly POSTGRESQL_JDBC_DRIVER=postgresql-42.1.4.jar
+readonly PAYARA_IP_ADDRESS=192.168.2.252
+readonly PAYARA=/payara41
+readonly PAYARA_BIN=${PAYARA}/bin
+readonly DB_USER_NAME=${APP_NAME}${APP_ID}
+readonly DB_NAME=${APP_NAME}${APP_ID}
 
-cd /payara41/bin/
-./asadmin undeploy censistant
-rm -r $HOME/censistant/
-./asadmin delete-auth-realm censistantRealm
-./asadmin delete-jdbc-resource jdbc/postgres_censistant
-./asadmin delete-jdbc-connection-pool postgres_censistant_pool
-rm ../glassfish/domains/domain1/lib/ext/postgresql-42.1.4.jar
-./asadmin stop-domain
-dropdb censistant
+echo 'Password per application server'
+ssh root@${PAYARA_IP_ADDRESS} -p ${PAYARA_EXTERNAL_SSH_PORT} '\
+sh '${PAYARA_BIN}'/asadmin undeploy '${APP_NAME}'; \
+rm -r ${HOME}/'${APP_NAME}'/; \
+sh '${PAYARA_BIN}'/asadmin delete-auth-realm '${APP_NAME}'Realm; \
+sh '${PAYARA_BIN}'/asadmin delete-jdbc-resource jdbc/postgres_'${APP_NAME}'; \
+sh '${PAYARA_BIN}'/asadmin delete-jdbc-connection-pool postgres_'${APP_NAME}'_pool; \
+rm '${PAYARA}'/glassfish/domains/domain1/lib/ext/'${POSTGRESQL_JDBC_DRIVER}'; \
+rm -r '${PAYARA}'/glassfish/domains/domain1/generated/*; \
+sh '${PAYARA_BIN}'/asadmin stop-domain'
 
-cd /payara41/bin/
-mkdir $HOME/censistant
-mkdir $HOME/censistant/images
-mkdir $HOME/censistant/documents
-wget -P ../glassfish/domains/domain1/lib/ext/ https://jdbc.postgresql.org/download/postgresql-42.1.4.jar
-createdb --owner=censistant --encoding=UTF8 censistant
-./asadmin start-domain
-./asadmin create-jdbc-connection-pool \
+echo 'Password per database server'
+ssh root@${PAYARA_IP_ADDRESS} -p ${DB_EXTERNAL_SSH_PORT} '\
+dropdb '${DB_NAME}'; \
+createdb --owner='${DB_NAME}' --encoding=UTF8 '${DB_USER_NAME}''
+
+echo 'Password per application server'
+scp -P ${PAYARA_EXTERNAL_SSH_PORT} ~/NetBeansProjects/${APP_NAME}/target/${APP_NAME}.war root@${PAYARA_IP_ADDRESS}:/root/
+
+echo 'Password per application server'
+ssh root@${PAYARA_IP_ADDRESS} -p ${PAYARA_EXTERNAL_SSH_PORT} '\
+mkdir ${HOME}/'${APP_NAME}'; \
+mkdir ${HOME}/'${APP_NAME}'/images; \
+mkdir ${HOME}/'${APP_NAME}'/documents; \
+wget -P '${PAYARA}'/glassfish/domains/domain1/lib/ext/ https://jdbc.postgresql.org/download/'${POSTGRESQL_JDBC_DRIVER}'; \
+sh '${PAYARA_BIN}'/asadmin start-domain; \
+sh '${PAYARA_BIN}'/asadmin create-jdbc-connection-pool \
 --datasourceclassname=org.postgresql.ds.PGSimpleDataSource \
 --restype=javax.sql.DataSource \
 --validationmethod=auto-commit \
 --allownoncomponentcallers=false \
 --nontransactionalconnections=false \
 --driverclassname=org.postgresql.Driver \
---property user=censistant:\
-password=PNpKjM19byxa6JBIKxFU:\
-databaseName=censistant:\
-serverName=127.0.0.1:\
+--property user='${DB_USER_NAME}':\
+password='${DB_USER_PASSWORD}':\
+databaseName='${DB_NAME}':\
+serverName='${DB_IP_ADDRESS}':\
 portNumber=5432:\
-url=jdbc\\:postgresql\\://127.0.0.1\\:5432/censistant \
-postgres_censistant_pool
-./asadmin create-jdbc-resource --connectionpoolid postgres_censistant_pool jdbc/postgres_censistant
-./asadmin create-auth-realm \
+url=jdbc\\:postgresql\\://'${DB_IP_ADDRESS}'\\:5432/'${DB_NAME}' \
+postgres_'${APP_NAME}'_pool; \
+sh '${PAYARA_BIN}'/asadmin create-jdbc-resource --connectionpoolid postgres_'${APP_NAME}'_pool jdbc/postgres_'${APP_NAME}'; \
+sh '${PAYARA_BIN}'/asadmin create-auth-realm \
 --classname com.sun.enterprise.security.auth.realm.jdbc.JDBCRealm \
 --property \
 jaas-context=jdbcRealm:\
-datasource-jndi=jdbc/postgres_censistant:\
+datasource-jndi=jdbc/postgres_'${APP_NAME}':\
 user-table=user_app:\
 user-name-column=user_name:\
 password-column=pass_word:\
@@ -48,9 +68,7 @@ digest-algorithm=SHA-256:\
 digestrealm-password-enc-algorithm=AES:\
 encoding=base64:\
 charset=UTF-8 \
-censistantRealm
-./asadmin stop-domain
-./asadmin start-domain
-./asadmin deploy /root/censistant.war
-
-exit
+'${APP_NAME}'Realm; \
+sh '${PAYARA_BIN}'/asadmin stop-domain; \
+sh '${PAYARA_BIN}'/asadmin start-domain; \
+sh '${PAYARA_BIN}'/asadmin deploy /root/'${APP_NAME}'.war'
