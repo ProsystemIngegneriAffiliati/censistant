78cfe04eb65ba8dd1e4c8ec3c389f1e3bf921ca1
Davide Mainardi <ingmainardi@live.com>
2018-01-24 18:22:24 +0100

    Cleaned updateApplication production script

--- src/main/resources/payara/production/updateApplication.sh
+++ src/main/resources/payara/production/updateApplication.sh
@@ -2,18 +2,12 @@
 
 readonly APP_ID=1
 readonly APP_NAME=censistant
-readonly DB_IP_ADDRESS=192.168.2.250
-readonly DB_NAME=${APP_NAME}${APP_ID}
-readonly DB_USER_NAME=${APP_NAME}${APP_ID}
-readonly POSTGRESQL_JDBC_DRIVER=postgresql-42.2.0.jar
 readonly AS_IP_ADDRESS=192.168.2.249
 readonly AS_EXTERNAL_SSH_PORT=22
 readonly AS_INSTALL_DIR=/opt
 readonly AS_ROOT_DIR=${AS_INSTALL_DIR}/payara41
 readonly AS_BIN=${AS_ROOT_DIR}/bin
-readonly AS_VERSION=4.1.2.174
 readonly AS_DOMAIN_NAME=payaradomain
-readonly AS_PASSWORD_ALIAS_NAME=${APP_NAME}-dbuser-alias
 
 echo 'Password per application server'
 scp -P ${AS_EXTERNAL_SSH_PORT} ~/NetBeansProjects/${APP_NAME}/target/${APP_NAME}.war root@${AS_IP_ADDRESS}:/root/
