f9da5e58373c8f04493d4d93848783b93cce2ce9
Davide Mainardi <ingmainardi@live.com>
2019-03-07 14:32:26 +0100

    Fixed the "address is removed when "is only info" new customer".

---
+++ src/main/java/com/prosystemingegneri/censistant/business/maintenance/entity2/MaintenanceContract2.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2019 Prosystem Ingegneri Affiliati.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.prosystemingegneri.censistant.business.maintenance.entity2;
+
+import com.prosystemingegneri.censistant.business.entity.BaseEntity;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Version;
+
+/**
+ *
+ * @author Davide Mainardi <ingmainardi at live.com>
+ */
+@Entity
+public class MaintenanceContract2 extends BaseEntity<Long> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Lob
+    private String notes;
+    
+    @Version
+    private int version;
+
+    public MaintenanceContract2() {
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+    
+}
